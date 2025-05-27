f73427ad7950bbf43356273ce6f79bdce06ab37b
Anitakushare <anitakushare1@gmail.com>
2025-05-27 10:16:35 +0530

    portfolio uploaded

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+    darkMode: 'class',
+  theme: {
+    screen:{
+      'ipadMini': '768px',
+  // => @media (min-width: 768px) { ... }
+    },
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 4s linear infinite',
+        'border': 'border 4s ease infinite',
+      },
+      keyframes: {
+        border: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+    },
+  },
+  },
+  plugins: [],
+}
